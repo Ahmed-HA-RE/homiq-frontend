51f448fbe49302e14dd2f6a8f3c2f55662fa9624
Ahmed-HA-RE <ah607k@gmail.com>
2025-08-31 22:48:20 +0400

    feat:add route method in the routes file to create route for projects and project details and remove the embla npm package

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from '@react-router/dev/routes';
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
-export default [index('routes/home/index.tsx')] satisfies RouteConfig;
+export default [
+  index('routes/home/index.tsx'),
+  route('/projects', './routes/projects/index.tsx'),
+  route('/project/:id', './routes/projects/details.tsx'),
+] satisfies RouteConfig;
